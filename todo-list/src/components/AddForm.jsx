0c42691c2fc4ecf52a84602823a5d0eabb09a218
Зульфугаров Фархад Самир <Zulfu_gy29@itstep.edu.az>
2023-11-22 21:40:17 +0400

    Added AddForm

---
+++ todo-list/src/components/AddForm.jsx
@@ -0,0 +1,24 @@
+import { Form } from "react-router-dom";
+import { addTask } from "../../redux/slices/tasksSlices";
+import { createTask } from "../../tasks";
+import { useDispatch } from "react-redux";
+
+export default function AddForm() {
+    const dispatch = useDispatch();
+
+    return (
+        <>
+            <Form method="post">
+                <button
+                    onClick={async () => {
+                        const task = await createTask();
+                        dispatch(addTask(task));
+                    }}
+                    type="submit"
+                >
+                    New
+                </button>
+            </Form>
+        </>
+    );
+}
